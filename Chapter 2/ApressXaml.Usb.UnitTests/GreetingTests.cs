2d7d2e9786628545c2604c8f77f71d875f1627c1
Barry Wallis <barry_wallis@acm.org>
2016-12-31 22:59:52 -0800

    Add ApressXaml.Usb and unit tests.

---
+++ Chapter 2/ApressXaml.Usb.UnitTests/GreetingTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: CLSCompliant(true)]
+namespace ApressXaml.Usb.UnitTests
+{
+    [TestClass]
+    public class GreetingTests
+    {
+        private IUserLogOnService userLoginService;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            userLoginService = new UserLogOnService();
+            Assert.IsNotNull(userLoginService);
+        }
+
+        [TestMethod]
+        public void IfNameLengthGreaterThan0ThenServiceReturnsMessage()
+        {
+            string validName = "Buddy";
+            string actualGreeting = userLoginService.GreetUser(validName);
+            string expectedGreeting = $"Hello {validName}!";
+
+            Assert.AreEqual(expectedGreeting, actualGreeting, "The user greeting is incorrect");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IfNameIsNullThenThrowArgumentNullException()
+        {
+            string validName = null;
+            userLoginService.GreetUser(validName);
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IfNameIsEmptyThenThrowArgumentNullException()
+        {
+            string validName = "";
+            userLoginService.GreetUser(validName);
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IfNameIsWhiteSpaceThenThrowArgumentNullException()
+        {
+            string validName = "   ";
+            userLoginService.GreetUser(validName);
+            Assert.Fail();
+        }
+    }
+}
---
+++ Chapter 2/ApressXaml.Usb/UserLoginService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: CLSCompliant(true)]
+namespace ApressXaml.Usb
+{
+    public class UserLogOnService : IUserLogOnService
+    {
+        public UserLogOnService()
+        {
+
+        }
+
+        public string GreetUser(string validName)
+        {
+            if (string.IsNullOrWhiteSpace(validName))
+                throw new ArgumentNullException(nameof(validName));
+
+            return $"Hello {validName}!";
+        }
+    }
+}
